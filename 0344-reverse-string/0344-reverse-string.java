1e3e071b15cd2758d45e1b6f0c62f498dc9fabc4
Sarath <sarathkumar98236@gmail.com>
2026-01-07 15:54:51 +0530

    Time: 0 ms (100%), Space: 48.5 MB (13.2%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,12 +1,30 @@
-class Solution {
-    public void reverseString(char[] s) {
-        ArrayList<Character> res = new ArrayList<>();
-        for(int i =0;i<s.length;i++){
-            res.add(s[i]);
+// class Solution {
+//     public void reverseString(char[] s) {
+//         ArrayList<Character> res = new ArrayList<>();
+//         for(int i =0;i<s.length;i++){
+//             res.add(s[i]);
+//         }
+//         Collections.reverse(res);
+//         for(int i=0;i<s.length;i++){
+//             s[i] = res.get(i);
+//         }
+//     }
+// }
+
+
+// below code using two pointers
+
+ class Solution {
+     public void reverseString(char[] s) {
+        int i=0;
+        int j= s.length-1;
+        int c = s.length;
+        while(i<j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
         }
-        Collections.reverse(res);
-        for(int i=0;i<s.length;i++){
-            s[i] = res.get(i);
-        }
-    }
-}
+     }
+ }
