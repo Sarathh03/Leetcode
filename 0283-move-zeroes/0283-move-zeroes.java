c24d93389743142841ea2e990071226a102f142f
Sarath <sarathkumar98236@gmail.com>
2026-01-21 19:18:27 +0530

    Time: 3 ms (20.2%), Space: 48 MB (26.92%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,40 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+
+
         int n = nums.length;
-        int count = 0;
-        ArrayList<Integer> ans =  new ArrayList<>();
-        for(int i =0;i<n;i++){
-            if(nums[i]!=0){
-                ans.add(nums[i]);
+        // int count = 0;
+        // ArrayList<Integer> ans =  new ArrayList<>();
+        // for(int i =0;i<n;i++){
+        //     if(nums[i]!=0){
+        //         ans.add(nums[i]);
+        //     }
+        // }
+        // while(ans.size()<n){
+        //     ans.add(0);
+        // }
+        // for(int i=0;i<n;i++){
+        //     nums[i]= ans.get(i);
+        // }
+        int i=0;
+        int z =1;
+        while(z<n){
+            if(nums[i]==0){
+                if(nums[z]!=0){
+                    int temp = nums[i];
+                    nums[i] = nums[z];
+                    nums[z] = temp; 
+                    i++;
+                    z++;
+                }else{
+                    z++;
+                }
+            }else{
+                i++;
+                z++;
             }
         }
-        while(ans.size()<n){
-            ans.add(0);
-        }
-        for(int i=0;i<n;i++){
-            nums[i]= ans.get(i);
-        }
+       
 
     }
 }
