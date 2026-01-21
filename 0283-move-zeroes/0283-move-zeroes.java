642e936a316e49a3a8af23ef396ef7ee741aaf5a
Sarath <sarathkumar98236@gmail.com>
2026-01-21 19:18:39 +0530

    Time: 1 ms (99.75%), Space: 47.7 MB (65.53%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -16,24 +16,36 @@ class Solution {
         // for(int i=0;i<n;i++){
         //     nums[i]= ans.get(i);
         // }
-        int i=0;
-        int z =1;
-        while(z<n){
-            if(nums[i]==0){
-                if(nums[z]!=0){
-                    int temp = nums[i];
-                    nums[i] = nums[z];
-                    nums[z] = temp; 
-                    i++;
-                    z++;
-                }else{
-                    z++;
-                }
-            }else{
-                i++;
+
+        int z=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                nums[z] = nums[i];
                 z++;
             }
         }
+        for(int i = z;i<n;i++){
+            nums[i] = 0;
+        }
+
+        // int i=0;
+        // int z =1;
+        // while(z<n){
+        //     if(nums[i]==0){
+        //         if(nums[z]!=0){
+        //             int temp = nums[i];
+        //             nums[i] = nums[z];
+        //             nums[z] = temp; 
+        //             i++;
+        //             z++;
+        //         }else{
+        //             z++;
+        //         }
+        //     }else{
+        //         i++;
+        //         z++;
+        //     }
+        // }
        
 
     }
