2a9f858149b9c501788fb6c7c69d7418bf2e0a9f
Sarath <sarathkumar98236@gmail.com>
2026-08-02 13:11:16 +0530

    Time: 3 ms (58.50%) | Memory: 45.1 MB (39.26%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,9 +1,34 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
-        StringBuilder sb = new StringBuilder(s);
-        String last = (sb.reverse()).toString();
-        if(s.equals(last))return true;
-        return false;
+        // s = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        // StringBuilder sb = new StringBuilder(s);
+        // String last = (sb.reverse()).toString();
+        // if(s.equals(last))return true;
+        // return false;
+        int st = 0;
+        int end = s.length()-1;
+        while(st<end){
+            if(Character.isLetter(s.charAt(st))&& Character.isLetter(s.charAt(end))){
+                if(Character.toLowerCase(s.charAt(st)) != Character.toLowerCase(s.charAt(end))){
+                    return false;
+                }
+                st+=1;
+                end -= 1;
+            }
+            else if(Character.isDigit(s.charAt(st))&& Character.isDigit(s.charAt(end))){
+                if(s.charAt(st)!= s.charAt(end)){
+                    return false;
+                }
+                st+=1;
+                end-=1;
+            }
+            else if(!Character.isLetter(s.charAt(st)) && !Character.isDigit(s.charAt(st)))
+                st+=1;
+            else if(!Character.isLetter(s.charAt(end)) && !Character.isDigit(s.charAt(end)))
+                end -=1;
+            else
+                return false;
+        }
+        return true;
     }
 }
